457348c03c2c00b9cb5b6ebcb696d700d2bec2d6
Unknown <szymon.gackowski@gmail.com>
2019-05-08 22:10:14 +0200

    Initial structure

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Sketch/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Sketch from './components/Sketch.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Sketch
   }
 }
 </script>
---
+++ src/components/Sketch.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <vue-p5 v-on="{setup, draw}"></vue-p5>
+    </div>
+</template>
+
+<script>
+    import VueP5 from 'vue-p5'
+
+    export default {
+        name: 'HelloWorld',
+        props: {
+            msg: String
+        },
+
+        components: {
+            VueP5
+        },
+
+        data() {
+            return {
+                bgColor: [100, 255, 100]
+            }
+        },
+
+        methods: {
+            setup(sketch) {
+                sketch.createCanvas(400, 400);
+                sketch.background(...this.bgColor);
+            },
+
+            draw(sketch) {
+                sketch.ellipse(50, 50, 80, 80);
+            }
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+
+</style>
