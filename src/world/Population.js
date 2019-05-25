c507a636151c4d52effa186a923871b4c94cdba3
Unknown <szymon.gackowski@gmail.com>
2019-05-25 12:36:02 +0200

    Population reproduce

--- src/world/Population.js
+++ src/world/Population.js
@@ -1,10 +1,12 @@
 import Genome from "../logic/Genome";
 import Specimen from "./Specimen";
+import {distance} from "../logic/MathHelper";
+import Reproduction from "../logic/Reproduction";
 
 export default class Population {
 
     static get initialPopNum() {
-        return 30;
+        return 50;
     }
 
     constructor(world) {
@@ -39,7 +41,6 @@ export default class Population {
             return specimen.isAlive;
         });
 
-
         for (let specimen of this.specimens) {
             specimen
                 .update()
@@ -49,4 +50,51 @@ export default class Population {
         return this;
     }
 
+    reproduce() {
+        let children = [];
+
+        for (let i = 1; i < this.specimens.length; i++) {
+            for (let j = i + 1; j < this.specimens.length; j++) {
+                let dist = distance(this.specimens[i].position, this.specimens[j].position);
+
+                if (dist < 10) {
+                    const fertilityChance = this.specimens[i].fertility * this.specimens[j].fertility;
+                    let chance = fertilityChance;
+
+                    while (Math.random() < chance) {
+
+                        if (!this.specimens[i].canReproduce || !this.specimens[j].canReproduce) {
+                            break;
+                        }
+
+                        let code = Reproduction.cross(this.specimens[i].genome, this.specimens[j].genome);
+
+                        code = Reproduction.mutate(code);
+
+                        if (code.length === this.specimens[i].genome.length) {
+                            children.push(new Specimen(
+                                Genome.fromBin(code),
+                                this.world.randomX(),
+                                this.world.randomY()
+                            ));
+
+                            this.specimens[i].reproduced();
+                            this.specimens[j].reproduced();
+                        }
+                        chance *= fertilityChance;
+                    }
+                }
+            }
+        }
+
+        for (let specimen of children) {
+            this.specimens.push(specimen);
+        }
+
+        return this;
+    }
+
+    get count() {
+        return this.specimens.length;
+    }
 }
